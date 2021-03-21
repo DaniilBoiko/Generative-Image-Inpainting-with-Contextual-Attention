1deef809bd150c2a18a2787b4503b30b72d09a10
DaniilBoiko <daniel.boyko.al@gmail.com>
2021-03-21 11:20:19 +0300

    Simpler branch

--- inpainting/train.py
+++ inpainting/train.py
@@ -21,30 +21,13 @@ class GAN(pl.LightningModule):
 
         self.coarse_network = CoarseNetwork(config['CoarseNetwork'])
 
-        self.refinement_network = RefinementNetwork(config['RefinementNetwork'])
-        self.refinement_network.cuda()
-
-        for _, layer in self.refinement_network.layers.items():
-            layer.cuda()
-
-        for layer in self.refinement_network.layers['Attention'].layers:
-            layer = layer.cuda()
-
-            if layer.__class__.__name__ == 'ContextualAttention':
-                layer.use_cuda = True
-
-        self.local_critic = LocalCritic(config['LocalCritic'])
         self.global_critic = GlobalCritic(config['GlobalCritic'])
-
-        self.l1_loss = torch.nn.L1Loss()
-
         self.bbox_size = bbox_size
 
     def forward(self, x):
         coarse_output = self.coarse_network(x)
-        refined_output = self.refinement_network(coarse_output)
 
-        return [coarse_output, refined_output]
+        return coarse_output
 
     def adversarial_loss(self, y_hat, y):
         return F.binary_cross_entropy(y_hat, y)
@@ -52,42 +35,42 @@ class GAN(pl.LightningModule):
     def training_step(self, imgs, batch_idx, optimizer_idx):
         bbox = random_bbox_fixed(64, 64, input_shape=(256, 256))
 
-        masks = torch.zeros(imgs.shape)
-        masks[:, :, bbox.left:bbox.right, bbox.bottom:bbox.top] += 1.0
-        masks = masks.float().cuda()
-
         x = imgs.float()
         x[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top] = 0.0
 
         spatial_dis = spatial_discount(0.999, (64, 64), True).cuda()
 
         cn_output = self.coarse_network(x)
-        rn_output = self.refinement_network(cn_output, masks)
 
-        x_cn = cn_output * masks + x * (1 - masks)
-        x_rn = rn_output * masks + x * (1 - masks)
+        x_cn = cn_output
+
+        if batch_idx % 100 == 0:
+            grid = make_grid(x)
+            self.logger.experiment.add_image('source', grid, batch_idx)
+
+            grid = make_grid(x_cn)
+            self.logger.experiment.add_image('output', grid, batch_idx)
+
+            grid = make_grid(imgs)
+            self.logger.experiment.add_image('gt', grid, batch_idx)
 
         if optimizer_idx == 0:
-            lc_preds_fake = self.local_critic(x_rn[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top])
-            gc_preds_fake = self.global_critic(x_rn)
-
-            lc_preds_real = self.local_critic(x[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top])
+            gc_preds_fake = self.global_critic(x_cn)
             gc_preds_real = self.global_critic(x)
 
-            loss = lc_preds_fake.mean() - lc_preds_real.mean() + gc_preds_fake.mean() - gc_preds_real.mean()
+            loss = gc_preds_fake.mean() - gc_preds_real.mean()
 
             self.log('D_loss', loss.item())
             return loss
 
         if optimizer_idx == 1:
             l1_losses = spatial_dis * (
-                (torch.abs(x_cn - x) + torch.abs(x_rn - x))[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top]
+                (torch.abs(x_cn - x))[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top]
             )
 
-            lc_preds_fake = self.local_critic(x_rn[:, :, bbox.left: bbox.right, bbox.bottom: bbox.top])
-            gc_preds_fake = self.global_critic(x_rn)
+            gc_preds_fake = self.global_critic(x_cn)
 
-            adversarial_losses = -lc_preds_fake.mean() - gc_preds_fake.mean()
+            adversarial_losses = - gc_preds_fake.mean()
 
             loss = l1_losses.mean() + adversarial_losses
 
