bc6958c1aa4aed345944f8a69f98fa84dbdb0d26
DaniilBoiko <daniel.boyko.al@gmail.com>
2021-03-24 18:23:28 +0300

    Created class

--- test.py
+++ test.py
@@ -3,35 +3,32 @@ import torch
 
 import numpy as np
 
-model = GAN.load_from_checkpoint('epoch=1-step=54749.ckpt')
-model = model.cuda()
 
+class Model:
+    def __init__(self, path):
+        self.model = GAN.load_from_checkpoint('epoch=1-step=54749.ckpt')
+        self.model = self.model.cuda()
 
-def get_predictions(image, mask, model, cheat=True):
-    """
-    Get predictions from the model
-    :param image: (H, W, C) with integers from 0 to 255
-    :param mask: (H, W) with 0 and 255 values
-    :param model: preloaded model
-    :param cheat: cheat or not
-    """
-    image = 2 * (np.array([np.moveaxis(image, -1, 0)]) / 255.0).astype(float) - 1
-    mask = (np.array([[mask]]) / 255.0).astype(float)
+    def forward(self, image, mask, cheat=True):
+        """
+        Get predictions from the model
+        :param image: (H, W, C) with integers from 0 to 255
+        :param mask: (H, W) with 0 and 255 values
+        :param model: preloaded model
+        :param cheat: cheat or not
+        :returns: (H, W, C)
+        """
+        image = 2 * (np.array([np.moveaxis(image, -1, 0)]) / 255.0).astype(float) - 1
+        mask = (np.array([[mask]]) / 255.0).astype(float)
 
-    image = torch.cuda.FloatTensor(image)
-    mask = torch.cuda.FloatTensor(mask)
+        image = torch.cuda.FloatTensor(image)
+        mask = torch.cuda.FloatTensor(mask)
 
-    if not cheat:
-        image = image * (1 - mask) + mask
+        if not cheat:
+            image = image * (1 - mask) + mask
 
-    with torch.no_grad():
-        outputs = model.forward(image, mask) * mask + image * (1 - mask)
-        outputs = outputs.cpu().numpy()
+        with torch.no_grad():
+            outputs = self.model.forward(image, mask) * mask + image * (1 - mask)
+            outputs = outputs.cpu().numpy()
 
-    return np.moveaxis((255 * (outputs[0] + 1) / 2).astype(np.uint8), 0, -1)
-
-
-image = (np.random.rand(256, 256, 3) * 255).astype(np.uint8)
-mask = (np.random.rand(256, 256) * 255).astype(np.uint8)
-
-outputs = get_predictions(image, mask, model)
+        return np.moveaxis((255 * (outputs[0] + 1) / 2).astype(np.uint8), 0, -1)
