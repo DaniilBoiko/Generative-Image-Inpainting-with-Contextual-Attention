13f02ca4a3043f54bdbd6835dfe2d52280b78be0
DaniilBoiko <daniel.boyko.al@gmail.com>
2021-03-24 16:02:18 +0300

    cleanup

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from inpainting.train import GAN
 import torch
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 model = GAN.load_from_checkpoint('epoch=1-step=54749.ckpt')
 model = model.cuda()
@@ -26,21 +25,3 @@ image = (np.random.rand(256, 256, 3) * 255).astype(np.uint8)
 mask = (np.random.rand(256, 256) * 255).astype(np.uint8)
 
 outputs = get_predictions(image, mask, model)
-
-plt.figure()
-plt.subplot(131)
-plt.imshow(image)
-plt.colorbar()
-plt.clim(0, 255)
-
-plt.subplot(132)
-plt.imshow(mask)
-plt.colorbar()
-plt.clim(0, 255)
-
-plt.subplot(133)
-plt.imshow(outputs)
-plt.colorbar()
-plt.clim(0, 255)
-
-plt.savefig('example.png')
